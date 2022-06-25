a39469eaa2920df283d90ba4de558913d217ebc2
Matheus Lucas <matheusinhoinho2@gmail.com>
2022-06-24 21:38:08 -0300

    header modificated
    Modificação nas posições do header

--- vidrelo-client/src/components/LandPage/Header/Header.tsx
+++ vidrelo-client/src/components/LandPage/Header/Header.tsx
@@ -5,37 +5,41 @@ import './Style/Header.scss'
 export default function Header(){
     return(
         <header>
-            <img src={Logo} alt="Logo" id='logo' />
-            <nav>
-                <ul>
-                    <li>
-                        <a href="#home">Home</a>
-                    </li>
-                    <li>
-                        <a href="#produtos">Produtos</a>
-                    </li>
-                    <li>
-                        <a href="#como-funciona">Como Funciona?</a>
-                    </li>
-                    <li>
-                        <a href="#fale-conosco">Fale Conosco</a>
-                    </li>         
-                </ul>
-            </nav>
-            <nav className='nav-buttons'>    
-                <ul>
-                    <li>
-                        <Link  to="/login">Entrar</Link>           
-                    </li>
-                    <li>
-                        <Link to="/Cadastro">Cadastrar</Link>
-                    </li>
+            
+            <div id='header-content'>
+                <img src={Logo} alt="Logo" id='logo' />
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="#home">Home</a>
+                        </li>
+                        <li>
+                            <a href="#produtos">Produtos</a>
+                        </li>
+                        <li>
+                            <a href="#como-funciona">Como Funciona?</a>
+                        </li>
+                        <li>
+                            <a href="#fale-conosco">Fale Conosco</a>
+                        </li>         
+                    </ul>
+                </nav>
+                <nav className='nav-buttons'>    
+                    <ul>
+                        <li>
+                            <Link  to="/login">Entrar</Link>           
+                        </li>
+                        <li>
+                            <Link to="/Cadastro">Cadastrar</Link>
+                        </li>
 
-                    <li>
-                        <Link to="/Carrinho"><img src={Carrinho} alt="carrinho" /></Link>
-                    </li>
-                </ul>
-            </nav>   
+                        <li>
+                            <Link to="/Carrinho"><img src={Carrinho} alt="carrinho" /></Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+               
         </header>
     )
 }
