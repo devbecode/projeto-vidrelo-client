ff7887a5c9743819d86ff5542530e4ededf72acd
Raphael Rocha <raphaelfsrocha@gmail.com>
2022-07-07 17:23:07 -0300

    Cards and backgroundimage
    Cards de informacoes e background com elo , ainda nao responsivo

--- vidrelo-client/src/components/SingleProduct.tsx
+++ vidrelo-client/src/components/SingleProduct.tsx
@@ -1,4 +1,5 @@
 import './styles/singleProduct.scss'
+import bg from '../assets/images/bg/bgelo.svg'
 import espelho from '../assets/images/produtos/espelho.svg'
 import polygon from '../assets/images/icons/polygon.svg'
 import wpicon from '../assets/images/icons/wpicon.svg'
@@ -8,9 +9,7 @@ export default function SingleProducts() {
     <>
       <section className='container_products'>
         <div className='products_img'>
-          <span className='img-box'>
-            <img src={espelho} alt='' />
-          </span>
+          <img src={espelho} alt='' />
         </div>
         <div className='products_body'>
           <h1 className=' products_title'>Espelhos</h1>
@@ -29,31 +28,49 @@ export default function SingleProducts() {
           </div>
         </div>
       </section>
-      <section className='product-info'></section>
       <section className='product_info'>
         <h3>Informações:</h3>
         <div className='container_info'>
           <ul>
             <li>
               <h4>Título Base</h4>
-              <p>Descrição</p>
+              <p>
+                Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit
+                aut fugit, sed quia consequuntur magni dolores eos qui ratione
+                voluptatem sequi nesciunt. Neque porro quisquam est, qui
+                dolorem.Nemo enim ipsam voluptatem quia voluptas sit aspernatur
+                aut odit aut fugit, sed quia consequuntur magni dolores eos qui
+                ratione.
+              </p>
             </li>
             <li>
               <h4>Título Base</h4>
-              <p>Descrição</p>
+              <p>
+                Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit
+                aut fugit, sed quia consequuntur magni dolores eos qui ratione
+                voluptatem sequi nesciunt. Neque porro quisquam est, qui
+                dolorem.Nemo enim ipsam voluptatem quia voluptas sit aspernatur
+                aut odit aut fugit, sed quia consequuntur magni dolores eos qui
+                ratione.
+              </p>
             </li>
             <li>
               <h4>Título Base </h4>
-              <p>Descrição</p>
+              <p>
+                Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit
+                aut fugit, sed quia consequuntur magni dolores eos qui ratione
+                voluptatem sequi nesciunt. Neque porro quisquam est, qui
+                dolorem.Nemo enim ipsam voluptatem quia voluptas sit aspernatur
+                aut odit aut fugit, sed quia consequuntur magni dolores eos qui
+                ratione.
+              </p>
             </li>
           </ul>
         </div>
       </section>
-      <section className='job_done'>
-        <h3>Veja alguns trabalhos que fizemos</h3>
-        <p>Carrosel</p>
+
+      <section className='img_elo'>
       </section>
-      <section className='img_elo'></section>
       <section className='question_info'>
         <h2>
           Tem alguma duvida? Entre em contato conosco que vamos lhe ajudar!
@@ -62,7 +79,7 @@ export default function SingleProducts() {
         <div className='container_contact_btn'>
           <button className='contact_btn'>Ligue agora: (81) 4042-0876</button>
           <button className='contact_btn'>
-            <img src={wpicon} alt='whatsapp' className='wp_icon'/>
+            <img src={wpicon} alt='whatsapp' className='wp_icon' />
             Fale pelo WhatsApp
           </button>
         </div>
